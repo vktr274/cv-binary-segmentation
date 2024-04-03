755cd30c8c4f72ea150e0ea9099b476b83e4d341
Viktor Modroczký <v.modroczky@gmail.com>
2024-04-03 15:42:34 +0200

    Add task 2 eval

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Computer Vision @ FIIT STU
 
 ## Task 1 - GrabCut
 
+The [`task1.ipynb`](task1.ipynb) notebook contains the code and visualizations for this task.
+
 Selected image: dog on a chair\
 Object of interest: the whole chair
 
@@ -30,6 +32,8 @@ The red area marks possible foreground, the whie area marks sure foreground, and
 
 ## Task 2 - Semi-supervised Segmentation
 
+The [`task2.ipynb`](task2.ipynb) notebook contains the implementation of the semi-supervised segmentation algorithm. The algorithm is also implemented in the [`utils.py`](utils.py) file as a function without visualization called `semisupervised_segmentation`. This version is then used for evaluation.
+
 ### Proposed algorithm
 
 1. Load image and blur it with a median filter of size 7x7.
@@ -44,17 +48,39 @@ The red area marks possible foreground, the whie area marks sure foreground, and
 7. Find the closest points to the selected points in array of local maxima.
 8. Compute the shortest path between these points using Dijkstra's algorithm. The result is shown below in comparison with the selected local maxima.\
 ![Shortest path](images/local_maxima_selected_found.png)
-9. Create mask of superpixels that the shortest path traverses. The result is shown below.\
+9. Create mask of superpixels (each dilated by a rectangular 11x11 structuring element in 1 iteration) that the shortest path traverses. The result is shown below.\
 ![Mask of superpixels](images/path_superpixels.png)
-10. Dilate the mask in 3 iterations using an 11x11 structuring element. The result is shown below.\
+10. Dilate the mask in 4 iterations using an 7x7 elliptic structuring element. The result is shown below.\
 ![Dilated mask](images/dilated_mask.png)
 11. Find contour of the mask and fill it. Compute XOR of the mask with the dilated mask which results in a mask with containing holes of the dilated mask. This way, we can mark the holes as sure foreground. The result is shown below.\
 ![Created mask](images/created_mask.png)
 12. Apply grabcut with the created mask and 5 iterations. The resulting mask is shown below.\
 ![After grabcut](images/mask_after_grabcut.png)
-13. Mark sure foreground and possible foreground as sure foreground. The result is shown below in comparison with the ground truth for one of the images.\
+13. Mark sure foreground and possible foreground as sure foreground. The result is shown below in comparison with the ground truth for one of the images. The DICE score between the mask after grabcut and ground truth mask is 91.74%.\
 ![Final mask](images/final_mask_2.png)
 14. Apply the mask to the original image. The result is shown below for one of the images.\
 ![Segmented image](images/segmented_2.png)
+
+### Algorithm Evaluation
+
+The [`segmentation_test.ipynb`](segmentation_test.ipynb) notebook contains the evaluation of the proposed algorithm. The algorithm is evaluated on 3 more images.
+
+#### Image 1
+
+The DICE score between the mask after grabcut and ground truth mask is 88.84%.\
+
+![Segmented image](images/eval_1.png)
+
+#### Image 2
+
+The DICE score between the mask after grabcut and ground truth mask is 93.58%.\
+
+![Segmented image](images/eval_2.png)
+
+#### Image 3
+
+The DICE score between the mask after grabcut and ground truth mask is 96.82%.\
+
+![Segmented image](images/eval_3.png)
 
 ## Task 3 - Moving Object Segmentation
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
+import copy
 import numpy as np
+import cv2
+from scipy.spatial import Delaunay
+import networkx as nx
+from networkx.algorithms.shortest_paths.weighted import dijkstra_path
 
 
 def merge_image_superpixels(image, mask, color):
@@ -20,3 +25,132 @@ def merge_image_superpixels(image, mask, color):
                 superpixel_contour_img[i, j] = image[i, j]
 
     return superpixel_contour_img
+
+
+def mouse_callback(event, x, y, _, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x_math = x
+        y_math = param[0].shape[0] - y
+        print(f"Left button clicked at ({x_math}, {y_math})")
+        param[1].append(np.array([x_math, y_math]))
+
+
+def semisupervised_segmentation(image):
+    """
+    Implements the algorithm proposed in the task2.ipynb notebook.
+    """
+    median_blurred = cv2.medianBlur(image, 7)
+    lab_img = cv2.cvtColor(median_blurred, cv2.COLOR_BGR2Lab)
+
+    superpixels_lsc = cv2.ximgproc.createSuperpixelLSC(lab_img, region_size=16)
+    superpixels_lsc.iterate(20)
+    superpixels_lsc_mask = superpixels_lsc.getLabelContourMask()
+
+    superpixel_contour_img = merge_image_superpixels(
+        image, superpixels_lsc_mask, [0, 255, 0]
+    )
+
+    superpixel_labels = superpixels_lsc.getLabels()
+    masks = []
+
+    for i in np.unique(superpixel_labels):
+        mask = np.where(superpixel_labels == i, 1, 0).astype(np.uint8)
+        masks.append(mask)
+
+    dist_trans = cv2.distanceTransform(~superpixels_lsc_mask, cv2.DIST_L2, 5)  # type: ignore
+
+    local_maxima_coord = []
+
+    for mask in masks:
+        masked = mask * dist_trans
+        local_maximum = np.argwhere(masked == np.max(masked))[0]
+        dist_trans[local_maximum[0], local_maximum[1]] = 1
+
+        local_maxima_coord.append(
+            np.array([local_maximum[1], (dist_trans.shape[0] - 1) - local_maximum[0]])
+        )
+
+    local_maxima_coord = np.array(local_maxima_coord)
+
+    trangulation = Delaunay(local_maxima_coord)
+    G = nx.Graph()
+    for path in trangulation.simplices:
+        nx.add_path(G, path)
+
+    clicks = []
+    cv2.imshow("Image", superpixel_contour_img)
+    cv2.setMouseCallback("Image", mouse_callback, (dist_trans, clicks))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    distances = []
+    nearest_to_clicks = []
+
+    for i, point_click in enumerate(clicks):
+        for j, point_maximum in enumerate(local_maxima_coord):
+            distances.append(np.array([i, j, cv2.norm(point_maximum, point_click)]))
+
+    distances = np.array(distances)
+
+    for i in range(len(clicks)):
+        interest_points = distances[distances[:, 0] == i]
+        minimum_dist = interest_points[
+            interest_points[:, 2] == np.min(interest_points[:, 2]), 1
+        ]
+        nearest_to_clicks.append(int(minimum_dist[0]))
+
+    indices_of_nearest = []
+
+    for coord_click in local_maxima_coord[nearest_to_clicks]:
+        for i, coord in enumerate(local_maxima_coord):
+            if (coord == coord_click).all():
+                indices_of_nearest.append(i)
+                break
+
+    indices_of_nearest = np.array(indices_of_nearest)
+    result = copy.deepcopy(local_maxima_coord[nearest_to_clicks])
+
+    for i in range(len(indices_of_nearest) - 1):
+        path = dijkstra_path(G, indices_of_nearest[i], indices_of_nearest[i + 1])
+        result = np.append(result, values=local_maxima_coord[path][1:-1], axis=0)  # type: ignore
+
+    path = dijkstra_path(G, indices_of_nearest[0], indices_of_nearest[-1])
+    result = np.append(result, values=local_maxima_coord[path][1:-1], axis=0)  # type: ignore
+
+    selected_superpixels = np.zeros(dist_trans.shape, np.uint8)
+    struct_elem = cv2.getStructuringElement(cv2.MORPH_RECT, (11, 11))
+
+    for mask in masks:
+        for local_max in result:
+            if mask[(dist_trans.shape[0] - 1) - local_max[1], local_max[0]] == 1:
+                selected_superpixels |= cv2.dilate(mask, struct_elem, iterations=1)  # type: ignore
+
+    struct_elem = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+    dilated_selection = cv2.dilate(selected_superpixels, struct_elem, iterations=4)
+
+    contours, _ = cv2.findContours(
+        dilated_selection, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+    mask = np.zeros(dilated_selection.shape, dtype=np.uint8)
+    mask = cv2.fillPoly(mask, contours, 1)  # type: ignore
+
+    dilated_selection[dilated_selection == 1] = cv2.GC_PR_FGD
+
+    certain_fg = np.logical_xor(mask, dilated_selection)
+    certain_fg[certain_fg == 1] = cv2.GC_FGD
+
+    created_mask = dilated_selection + certain_fg
+    grabcut_mask = dilated_selection + certain_fg
+
+    bgd_model = np.zeros((1, 65), np.float64)
+    fgd_model = np.zeros((1, 65), np.float64)
+    grabcut_mask, bgd_model, fgd_model = cv2.grabCut(
+        image, grabcut_mask, None, bgd_model, fgd_model, 5, cv2.GC_INIT_WITH_MASK  # type: ignore
+    )
+    grabcut_mask = np.where(
+        (grabcut_mask == cv2.GC_FGD) | (grabcut_mask == cv2.GC_PR_FGD), 1, 0
+    )
+
+    segmented = image * cv2.merge([grabcut_mask, grabcut_mask, grabcut_mask])
+
+    return segmented, grabcut_mask, created_mask
